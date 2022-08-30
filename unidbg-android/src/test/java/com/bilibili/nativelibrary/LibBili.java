a9ff427d5bb0bdabd8b8e90cf9c2b8ccb888f0b0
yu <15170719135@163.com>
2022-08-31 00:13:55 +0800

    update-家

--- unidbg-android/src/test/java/com/bilibili/nativelibrary/LibBili.java
+++ unidbg-android/src/test/java/com/bilibili/nativelibrary/LibBili.java
@@ -8,6 +8,7 @@ import com.github.unidbg.linux.android.AndroidEmulatorBuilder;
 import com.github.unidbg.linux.android.AndroidResolver;
 import com.github.unidbg.linux.android.dvm.*;
 import com.github.unidbg.linux.android.dvm.api.PackageInfo;
+import com.github.unidbg.linux.android.dvm.array.ByteArray;
 import com.github.unidbg.linux.android.dvm.jni.ProxyDvmObject;
 import com.github.unidbg.memory.Memory;
 import com.github.unidbg.utils.Inspector;
@@ -17,6 +18,9 @@ import com.sun.jna.Pointer;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class LibBili extends AbstractJni {
     private final AndroidEmulator emulator; //AndroidARM64Emulator
     private final VM vm;
@@ -54,11 +58,17 @@ public class LibBili extends AbstractJni {
         List<Object> list = new ArrayList<>();
         list.add(vm.getJNIEnv());
         list.add(0);
-        DvmObject<?> context = vm.resolveClass("com/mfw/roadbook/MainApplication").newObject(null);
+        DvmObject<?> context = vm.resolveClass("com.bilibili.nativelibrary.LibBili").newObject(null);
         list.add(vm.addLocalObject(context));
         list.add(vm.addLocalObject(new StringObject(vm,"GET&https%3A%2F%2Fmapi.mafengwo.cn%2Fsearch%2Fsearch%2Fget_result_list%2Fv3&app_code%3Dcom.mfw.roadbook%26app_ver%3D10.8.0%26app_version_code%3D1004%26brand%3Dgoogle%26channel_id%3DZhiHuiYun%26dev_ver%3DD2213.0%26device_id%3D4b1a056b0efac3f3%26device_mid%3D352531082210616%26device_type%3Dandroid%26hardware_model%3DPixel%26has_notch%3D0%26jsondata%3D%257B%2522switched_search_type_by_user%2522%253A0%252C%2522page%2522%253A%257B%2522boundary%2522%253A%25220%2522%252C%2522num%2522%253A10%257D%252C%2522keyword%2522%253A%2522%25E9%259D%2592%25E5%25B2%259B%2522%252C%2522is_correct%2522%253A%25220%2522%252C%2522search_id%2522%253A%25222c5b5364-e178-47ce-b7ed-fc4709e52594%2522%257D%26mfwsdk_ver%3D20140507%26o_coord%3Dgcj%26o_lat%3D30.166782%26o_lng%3D120.160189%26oauth_consumer_key%3D5%26oauth_nonce%3Dca81057d-e742-469c-ac41-95558f85294d%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1659968712%26oauth_token%3D0_0969044fd4edf59957f4a39bce9200c6%26oauth_version%3D1.0%26open_udid%3D4b1a056b0efac3f3%26screen_height%3D1794%26screen_scale%3D2.88%26screen_width%3D1080%26sys_ver%3D8.1.0%26time_offset%3D480%26x_auth_mode%3Dclient_auth")));
         list.add(vm.addLocalObject(new StringObject(vm,"com.mfw.roadbook")));
-        Number number = module.callFunction(emulator, 0x396c8, list.toArray()); //上面日志会打印这个方法地址
+
+        TreeMap<String,String> treeMap = new TreeMap<>();
+        treeMap.put("s","xxx");
+        list.add(vm.addLocalObject(vm.resolveClass("java.util.TreeMap").newObject(treeMap)) );
+        list.add(vm.addLocalObject(new ByteArray(vm,new byte[]{0,0,0,1})));
+
+        Number number = module.callFunction(emulator, 0x1c97, list.toArray()); //上面日志会打印这个方法地址
         DvmObject<?> obj = ProxyDvmObject.createObject(vm, this);
 
         System.out.println(vm.getObject(number.intValue()).getValue().toString());
